4196bee21f62eb05c7235a4e79528bf4c40bdebe
FBI <chupacabra2.0666@gmail.com>
2024-10-07 20:39:41 +0300

    fix: fix __dirname is not defined

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,8 +1,12 @@
 import { promises as fs } from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const create = async () => {
-const filePath = path.join(__dirname, 'files', 'fresh.txt');
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');
 
     try {
         await fs.access(filePath);
