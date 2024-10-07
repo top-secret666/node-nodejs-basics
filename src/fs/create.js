8eb604fe83eaf7053a348843c247f03eb04c5e70
FBI <chupacabra2.0666@gmail.com>
2024-10-07 20:36:08 +0300

    fix: changing require to import

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,5 @@
-const fs = require('fs').promises;
-const path = require('path');
+import { promises as fs } from 'fs';
+import path from 'path';
 
 const create = async () => {
 const filePath = path.join(__dirname, 'files', 'fresh.txt');
